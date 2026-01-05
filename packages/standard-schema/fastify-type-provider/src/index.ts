b1dcc47368a7450e6f3601c721732d251a7e017d
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-05 20:42:25 -0300

    feat(standard-schema/fastify-type-provider): crie e exporte um tipo de intância do fastify
    O pacote não exporta um tipo da instância do fastify com o
    'StandardSchemaTypeProvider' e acaba sendo necessário declarar esse tipo
    toda vez que for preciso usá-lo
    
    Por isso o tipo 'FastifyStandardSchema' foi criado para evitar
    redundância de código e poder ser facilmente importado quando necessário

--- packages/standard-schema/fastify-type-provider/src/index.ts
+++ packages/standard-schema/fastify-type-provider/src/index.ts
@@ -1,4 +1,8 @@
-export type { JsonSchemaTransformFn, StandardSchemaTypeProvider } from './types'
+export type {
+    JsonSchemaTransformFn,
+    StandardSchemaTypeProvider,
+    FastifyStandardSchema
+} from './types'
 
 export { StandardSchemaSerializerCompiler } from './serializer'
 export { StandardSchemaValidatorCompiler } from './validator'
--- packages/standard-schema/fastify-type-provider/src/types.ts
+++ packages/standard-schema/fastify-type-provider/src/types.ts
@@ -1,6 +1,13 @@
 import type { StandardSchemaV1 } from '@standard-schema/spec'
 
-import type { FastifyTypeProvider } from 'fastify'
+import type {
+    FastifyTypeProvider,
+    FastifyInstance,
+    RawServerDefault,
+    RawRequestDefaultExpression,
+    FastifyBaseLogger,
+    RawReplyDefaultExpression
+} from 'fastify'
 
 export interface StandardSchemaTypeProvider extends FastifyTypeProvider {
     validator: this['schema'] extends StandardSchemaV1
@@ -11,6 +18,14 @@ export interface StandardSchemaTypeProvider extends FastifyTypeProvider {
         ? NonNullable<this['schema']['~standard']['types']>['input']
         : unknown
 }
+
+export type FastifyStandardSchema = FastifyInstance<
+    RawServerDefault,
+    RawRequestDefaultExpression<RawServerDefault>,
+    RawReplyDefaultExpression<RawServerDefault>,
+    FastifyBaseLogger,
+    StandardSchemaTypeProvider
+>
 
 export type JsonSchemaTransformFn<TSchema = unknown> = (
     schema: unknown,
