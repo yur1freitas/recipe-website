64700afdc365132554a9007142741b6f2da6d617
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-13 18:51:17 -0300

    test(core/time): atualize o mock `randTime` para gerar número de dias e semanas
    Referente aos commits e919a017a1024ba69fc173e1b91d8589c23ce6a1 e
    4c94ecbdf03184de4dda199c2f60047f66239003, o mock 'randTime()' foi
    atualizado para gerar o número de dias e de semanas

--- packages/core/time/mocks/randTime.ts
+++ packages/core/time/mocks/randTime.ts
@@ -1,9 +1,11 @@
 import type { TimeInput } from '~/types'
 
+import { randWeeks } from './randWeeks'
 import { randSeconds } from './randSeconds'
 import { randMinutes } from './randMinutes'
 import { randMilliseconds } from './randMilliseconds'
 import { randHours } from './randHours'
+import { randDays } from './randDays'
 
 export type RandTimeOptions = TimeInput
 
@@ -12,6 +14,8 @@ export function randTime(options?: RandTimeOptions): Required<TimeInput> {
         ms: options?.ms ?? randMilliseconds(),
         second: options?.second ?? randSeconds(),
         minute: options?.minute ?? randMinutes(),
-        hour: options?.hour ?? randHours()
+        hour: options?.hour ?? randHours(),
+        day: options?.day ?? randDays(),
+        week: options?.week ?? randWeeks()
     }
 }
