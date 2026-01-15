58fc1787fa5f35859a1c92e728349e518d5c470a
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-15 17:48:55 -0300

    feat(ui/form): crie o componente `FormPassword`

--- packages/ui/form/src/components/Form/index.ts
+++ packages/ui/form/src/components/Form/index.ts
@@ -2,17 +2,20 @@ import type { FormSubmitProps } from './FormSubmit'
 import type { FormFieldProps } from './FormField'
 import type { FormInputProps } from './FormInput'
 import type { FormLabelProps } from './FormLabel'
+import type { FormPasswordProps } from './FormPassword'
 
 import { FormField } from './FormField'
 import { FormSubmit } from './FormSubmit'
 import { FormInput } from './FormInput'
 import { FormLabel } from './FormLabel'
+import { FormPassword } from './FormPassword'
 
 export const Form = {
     Field: FormField,
     Label: FormLabel,
     Input: FormInput,
-    Submit: FormSubmit
+    Submit: FormSubmit,
+    Password: FormPassword
 }
 
 export namespace FormProps {
@@ -20,4 +23,5 @@ export namespace FormProps {
     export type Label = FormLabelProps
     export type Input = FormInputProps
     export type Submit = FormSubmitProps
+    export type Password = FormPasswordProps
 }
