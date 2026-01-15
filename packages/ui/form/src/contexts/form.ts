e1554b731c5863c283c1318a635666ac42acbde4
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-15 17:44:49 -0300

    feat(ui/form): crie o componente `FormField`

---
+++ packages/ui/form/src/contexts/form.ts
@@ -0,0 +1,4 @@
+import { createFormHookContexts } from '@tanstack/react-form'
+
+export const { formContext, fieldContext, useFormContext, useFieldContext } =
+    createFormHookContexts()
---
+++ packages/ui/form/src/index.ts
@@ -0,0 +1,8 @@
+export type { FormProps } from './components/Form'
+export { Form } from './components/Form'
+export {
+    fieldContext,
+    formContext,
+    useFieldContext,
+    useFormContext
+} from './contexts/form'
