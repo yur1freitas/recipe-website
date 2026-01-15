87efb831be7554188e6c669be1081409efc75ce1
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-15 17:47:47 -0300

    feat(ui/form): crie o componente `FormLabel`

--- packages/ui/form/src/components/Form/index.ts
+++ packages/ui/form/src/components/Form/index.ts
@@ -1,19 +1,23 @@
 import type { FormSubmitProps } from './FormSubmit'
 import type { FormFieldProps } from './FormField'
 import type { FormInputProps } from './FormInput'
+import type { FormLabelProps } from './FormLabel'
 
 import { FormField } from './FormField'
 import { FormSubmit } from './FormSubmit'
 import { FormInput } from './FormInput'
+import { FormLabel } from './FormLabel'
 
 export const Form = {
     Field: FormField,
+    Label: FormLabel,
     Input: FormInput,
     Submit: FormSubmit
 }
 
 export namespace FormProps {
     export type Field = FormFieldProps
+    export type Label = FormLabelProps
     export type Input = FormInputProps
     export type Submit = FormSubmitProps
 }
