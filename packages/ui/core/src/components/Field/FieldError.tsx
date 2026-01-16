ec7e5807ffaf8dc9a38aaa9c5b988b11df41a4ed
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-16 20:37:53 -0300

    refactor(ui/core): reuse `Error` em `FieldError`

--- packages/ui/core/src/components/Field/FieldError.tsx
+++ packages/ui/core/src/components/Field/FieldError.tsx
@@ -1,13 +1,9 @@
 import { Field as BaseField } from '@base-ui/react/field'
-import { cx } from 'tailwind-variants/utils'
+
+import { Error } from '../Typography/Error'
 
 export type FieldErrorProps = BaseField.Error.Props & { className?: string }
 
-export function FieldError({
-    className,
-    ...props
-}: FieldErrorProps): React.JSX.Element {
-    const classNames = cx('field-error', className)
-
-    return <BaseField.Error {...props} render={<em />} className={classNames} />
+export function FieldError(props: FieldErrorProps): React.JSX.Element {
+    return <BaseField.Error render={Error} {...props} />
 }
