6fb176ca633214529dac8331fdf9d55623e5fe48
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-16 14:39:42 -0300

    chore(ui/captcha): configure o storybook no pacote
    O storybook permite visualizar, documentar e testar componentes
    em react
    
    Então ele foi adicionado para facilitar o desenvolvimento do pacote
    em relação à mudanças de design ou testar funcionalidades

---
+++ packages/ui/captcha/vitest.config.ts
@@ -0,0 +1,34 @@
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
+import { playwright } from '@vitest/browser-playwright'
+
+import { defineConfig } from 'vitest/config'
+
+const dirName = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    test: {
+        projects: [
+            {
+                extends: true,
+                plugins: [
+                    storybookTest({
+                        configDir: join(dirName, '.storybook')
+                    })
+                ],
+                test: {
+                    name: 'storybook',
+                    browser: {
+                        enabled: true,
+                        headless: true,
+                        provider: playwright(),
+                        instances: [{ browser: 'chromium' }]
+                    },
+                    setupFiles: ['.storybook/vitest.setup.ts']
+                }
+            }
+        ]
+    }
+})
