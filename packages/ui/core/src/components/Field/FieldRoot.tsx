1a4ca1fa0fb08e645c5765a6e8383e48231f15c0
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-16 19:31:21 -0300

    feat(ui/core): adicione estilização para o `FieldRoot`
    O componente 'FieldRoot' abrange tanto o 'FieldLabel' e o 'FieldControl'
    onde ambos deveriam ter um espaçamento entre si, mas não possuem
    
    Por isso o 'FieldRoot' recebeu um classe que aplica um flex box para
    espaçar o 'FieldLabel' do 'FieldControl'

--- packages/ui/core/src/components/Field/FieldRoot.tsx
+++ packages/ui/core/src/components/Field/FieldRoot.tsx
@@ -1,4 +1,13 @@
 import { Field as BaseField } from '@base-ui/react/field'
+import { cx } from 'tailwind-variants/utils'
 
-export type FieldRootProps = BaseField.Root.Props
-export const FieldRoot = BaseField.Root
+export type FieldRootProps = BaseField.Root.Props & { className?: string }
+
+export function FieldRoot({
+    className,
+    ...props
+}: FieldRootProps): React.JSX.Element {
+    const classNames = cx('field', className)
+
+    return <BaseField.Root {...props} className={classNames} />
+}
