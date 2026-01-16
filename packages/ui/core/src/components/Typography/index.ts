f53473ca6b93e5c130cc89f5aa05cfeecf2584a4
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-16 19:05:10 -0300

    feat(ui/core): crie o componente de tipografia `Link`

--- packages/ui/core/src/components/Typography/index.ts
+++ packages/ui/core/src/components/Typography/index.ts
@@ -1,6 +1,7 @@
 import type { ParagraphProps } from './Paragraph'
 import type { SmallProps } from './Small'
 import type { ListProps } from './List'
+import type { LinkProps } from './Link'
 import type { H3Props } from './H3'
 import type { H2Props } from './H2'
 import type { H1Props } from './H1'
@@ -8,6 +9,7 @@ import type { H1Props } from './H1'
 import { Paragraph } from './Paragraph'
 import { Small } from './Small'
 import { List } from './List'
+import { Link } from './Link'
 import { H3 } from './H3'
 import { H2 } from './H2'
 import { H1 } from './H1'
@@ -17,6 +19,7 @@ export const Typography = {
     H2,
     H3,
     List,
+    Link,
     Small,
     Paragraph
 }
@@ -26,6 +29,7 @@ export namespace TypographyProps {
     export type H2 = H2Props
     export type H3 = H3Props
     export type List = ListProps
+    export type Link = LinkProps
     export type Small = SmallProps
     export type Paragraph = ParagraphProps
 }
