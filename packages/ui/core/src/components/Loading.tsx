7e75d0a8d1b910566451db8aa92e4e0b4b8161e7
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-16 13:11:39 -0300

    feat(ui/core): adicione opção de definir a posição do rótulo no `Loading`

--- packages/ui/core/src/components/Loading.tsx
+++ packages/ui/core/src/components/Loading.tsx
@@ -1,12 +1,32 @@
+import type { VariantProps } from 'tailwind-variants/lite'
+
 import { LoaderCircleIcon } from 'lucide-react'
+import { tv } from 'tailwind-variants/lite'
 
-export interface LoadingProps extends React.ComponentProps<'svg'> {
-    label?: string
-}
+const loading = tv({
+    base: 'loading',
+    variants: {
+        labelPos: {
+            top: 'loading-label-top',
+            bottom: 'loading-label-bottom',
+            left: 'loading-label-left',
+            right: 'loading-label-right'
+        }
+    }
+})
 
-export function Loading({ label, ...props }: LoadingProps): React.JSX.Element {
+export type LoadingProps = React.ComponentProps<'svg'> &
+    VariantProps<typeof loading> & { label?: string }
+
+export function Loading({
+    label,
+    labelPos,
+    ...props
+}: LoadingProps): React.JSX.Element {
+    const classNames = loading({ labelPos })
+
     return (
-        <div className='loading'>
+        <div className={classNames}>
             <LoaderCircleIcon {...props} />
             {label && <small>{label}</small>}
         </div>
