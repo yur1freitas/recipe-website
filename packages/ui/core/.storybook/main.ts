20bd8b906ec432148fd40d98dd59a0ab736b4e56
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-16 14:12:13 -0300

    chore(ui/core): configure o storybook no pacote
    O storybook permite visualizar, documentar e testar componentes
    em react
    
    Então ele foi adicionado para facilitar o desenvolvimento do pacote
    em relação à mudanças de design ou testar funcionalidades

---
+++ packages/ui/core/.storybook/main.ts
@@ -0,0 +1,38 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+import type { UserConfig } from 'vite'
+
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import { mergeConfig } from 'vite'
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+const getAbsolutePath = (value: string): string => {
+    return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+}
+
+const cwd = process.cwd()
+
+const viteConfig: UserConfig = {
+    resolve: {
+        alias: {
+            '~': join(cwd, 'src/')
+        }
+    }
+}
+
+const config: StorybookConfig = {
+    viteFinal: (config) => mergeConfig(config, viteConfig),
+    stories: ['../src/**/*.stories.@(ts|tsx)'],
+    addons: [
+        getAbsolutePath('@chromatic-com/storybook'),
+        getAbsolutePath('@storybook/addon-vitest'),
+        getAbsolutePath('@storybook/addon-a11y'),
+        getAbsolutePath('@storybook/addon-docs')
+    ],
+    framework: getAbsolutePath('@storybook/react-vite')
+}
+
+export default config
