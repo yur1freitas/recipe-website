3657173eb01abb51e70acacfcb84e1e55cd4ae95
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-16 23:39:59 -0300

    fix(ui/core): remova a recursividade do componente `Link`
    O componente 'Link' retorna uma chamada de si mesmo, mas era para ser
    usado a tag '<a>' e não '<Link>'
    
    Então o retorno foi alterado, removendo a recursividade para previnir
    futuros problemas

--- packages/ui/core/src/components/Typography/Link.tsx
+++ packages/ui/core/src/components/Typography/Link.tsx
@@ -2,8 +2,16 @@ import { cx } from 'tailwind-variants/utils'
 
 export type LinkProps = React.ComponentProps<'a'>
 
-export function Link({ className, ...props }: LinkProps): React.JSX.Element {
+export function Link({
+    className,
+    children,
+    ...props
+}: LinkProps): React.JSX.Element {
     const classNames = cx('typography-link', className)
 
-    return <Link className={classNames} {...props} />
+    return (
+        <a className={classNames} {...props}>
+            {children}
+        </a>
+    )
 }
