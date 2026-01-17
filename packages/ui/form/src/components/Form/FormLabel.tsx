9d256c4bf68f6b0ff3a950bdebeba7941547a925
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-17 11:13:34 -0300

    fix(ui/form): não defina o `htmlFor` usando `name` em `FormLabel`
    Ao usar o componente 'Field.Label' ele recebe automaticamente o id
    do 'Field.Control'. Além disso, passar o 'name' como o 'htmlFor' está
    completamente errado nesse caso, pois não havia garantia que o 'id'
    seria igual ao 'name' e esse era o problema de acessibilidade que estava
    ocorrendo
    
    Então a definição do 'htmlFor' usando o Tanstack Form foi removido
    deixando o contexto do componente defini-lo automaticamente

--- packages/ui/form/src/components/Form/FormLabel.tsx
+++ packages/ui/form/src/components/Form/FormLabel.tsx
@@ -2,19 +2,8 @@ import type { FieldProps } from '@ui/core/Field'
 
 import { Field } from '@ui/core/Field'
 
-import { useFieldContext } from '~/contexts/form'
-
 export type FormLabelProps = FieldProps.Label
 
-export function FormLabel({
-    children,
-    ...props
-}: FormLabelProps): React.JSX.Element {
-    const field = useFieldContext<string>()
-
-    return (
-        <Field.Label {...props} htmlFor={field.name}>
-            {children}
-        </Field.Label>
-    )
+export function FormLabel(props: FormLabelProps): React.JSX.Element {
+    return <Field.Label {...props} />
 }
