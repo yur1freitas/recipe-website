da848791aa2b927d89739802a478ba71490086e1
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-18 11:18:01 -0300

    feat(ui/core): crie o hook `useTime`
    O hook 'useTime' pode facilitar a manipulação da classe 'Time' no react,
    como evitar ter que usar 'useState' toda vez que for necessário usá-la
    
    Então o hook foi criado para ser um utilitário para os componentes
    do '@ui/core' e também para outros que possam vim no futuro

---
+++ packages/ui/core/src/hooks/useTime.ts
@@ -0,0 +1,44 @@
+import type { TimeInput } from '@core/time'
+import { Time } from '@core/time'
+import { useState } from 'react'
+
+export type SetTimeFn = (time: Time) => void
+export type UpdateTimeFn = (updater: (time: Time) => Time) => void
+export type ChangeValueHandler = (time: Time) => void
+
+export interface UseTimeOutput {
+    time: Time
+    setTime: SetTimeFn
+    updateTime: UpdateTimeFn
+}
+
+export interface UseTimeInput extends TimeInput {
+    onChangeTime?: ChangeValueHandler
+}
+
+export function useTime({
+    onChangeTime,
+    ...props
+}: UseTimeInput = {}): UseTimeOutput {
+    const [time, setTimeState] = useState(() => new Time(props))
+
+    const setTime: SetTimeFn = (time) => {
+        onChangeTime?.(time)
+        setTimeState(time)
+    }
+
+    const updateTime: UpdateTimeFn = (updater) => {
+        setTimeState((prev) => {
+            const updated = updater(prev.clone())
+            onChangeTime?.(updated)
+
+            return updated
+        })
+    }
+
+    return {
+        time,
+        setTime,
+        updateTime
+    }
+}
