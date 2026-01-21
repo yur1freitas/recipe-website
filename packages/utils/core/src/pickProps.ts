1721cfa3b4bdf3e0eef2eaf11ea8452abf29e806
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-21 20:45:14 -0300

    feat(utils/core): crie o utilitário `pickProps`

---
+++ packages/utils/core/src/pickProps.ts
@@ -0,0 +1,20 @@
+export type AnyObject = Record<PropertyKey, any>
+
+export function pickProps<
+    TObject extends AnyObject,
+    TKeys extends keyof TObject
+>(object: TObject, keys: TKeys[]): Pick<TObject, TKeys> {
+    const out: AnyObject = {}
+
+    for (const key of keys) {
+        if (Object.hasOwn(object, key)) {
+            const value = object[key]
+
+            if (typeof value !== 'undefined') {
+                out[key] = value
+            }
+        }
+    }
+
+    return out as TObject
+}
