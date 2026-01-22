e5528d737110fc2824b8d8bdb1746a0afb0a3dca
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-22 18:17:49 -0300

    chore(ui/core): mova a config do vite em `.storybook/main.ts` para `vite.config.ts`

--- packages/ui/core/vite.config.ts
+++ packages/ui/core/vite.config.ts
@@ -1,10 +1,19 @@
+import { join } from 'node:path'
+
 import { defineConfig } from 'vite'
 
 import babel from '@rollup/plugin-babel'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
+const cwd = process.cwd()
+
 export default defineConfig({
+    resolve: {
+        alias: {
+            '~': join(cwd, 'src/')
+        }
+    },
     plugins: [
         tailwind(),
         react(),
