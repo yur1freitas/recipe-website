67a417503338c1137d3907f5cb69bfd575803023
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-22 13:56:09 -0300

    feat(ui/core): crie e exponha `SelectValueType`
    O Select do Base UI possui um tipo que não é exportado, que é o
    'SelectValueType' que define se o valor será um array ou não
    
    Então por necessidade, o tipo foi replicado em 'SelectRoot' e exportado
    para que possa ser usado

--- packages/ui/core/src/components/Select/SelectRoot.tsx
+++ packages/ui/core/src/components/Select/SelectRoot.tsx
@@ -1,5 +1,10 @@
 import { Select as BaseSelect } from '@base-ui/react/select'
 
+export type SelectValueType<
+    TValue,
+    TMultiple extends boolean | undefined = false
+> = TMultiple extends true ? TValue[] : TValue
+
 export type SelectRootProps<
     Value,
     Multiple extends boolean | undefined = false
