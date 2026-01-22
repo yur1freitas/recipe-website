f7b7b95af25d9959f77a374e2dda166a6a3ef891
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-21 22:13:48 -0300

    feat(utils/core): crie o utilitário `isPlainObject`
    Esse utilitário existe em '@standard-schema/fastify-type-provider' e
    está sendo migrado/reescrito nesse pacote

---
+++ packages/utils/core/src/isPlainObject.ts
@@ -0,0 +1,11 @@
+export type AnyObject = Record<PropertyKey, any>
+
+export function isPlainObject(input: unknown): input is AnyObject {
+    if (typeof input !== 'object' || Array.isArray(input) || input === null) {
+        return false
+    }
+
+    const proto = Object.getPrototypeOf(input)
+
+    return proto === Object.prototype || proto === null
+}
