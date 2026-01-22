1f5ca3e480b4d49107a9cd87094f144807e97b46
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-22 18:17:49 -0300

    chore(ui/core): mova a config do vite em `.storybook/main.ts` para `vite.config.ts`

--- packages/ui/core/.storybook/main.ts
+++ packages/ui/core/.storybook/main.ts
@@ -1,9 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite'
-import type { UserConfig } from 'vite'
 
-import { dirname, join } from 'node:path'
+import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { mergeConfig } from 'vite'
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -13,18 +11,7 @@ const getAbsolutePath = (value: string): string => {
     return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
 }
 
-const cwd = process.cwd()
-
-const viteConfig: UserConfig = {
-    resolve: {
-        alias: {
-            '~': join(cwd, 'src/')
-        }
-    }
-}
-
 const config: StorybookConfig = {
-    viteFinal: (config) => mergeConfig(config, viteConfig),
     stories: ['../src/**/*.stories.@(ts|tsx)'],
     addons: [
         getAbsolutePath('@chromatic-com/storybook'),
