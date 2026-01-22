e6eafb3efd49b28dade0d4267c2c11526ab70e18
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-22 16:11:39 -0300

    feat(ui/core)!: permita passar o valor de `useTime` e `TimeFieldsetRoot` através de uma única propriedade
    No hook 'useTime' o valor era definido através de diversas propriedades
    referente ao 'TimeInput', mas com a atualização da classe 'Time' (e8a1dccbdf9bb0765612e9a09cf425fa39f52931)
    seria melhor definir o valor através de uma única propriedade que poderia
    ser 'TimeInput' ou 'Time', consequentemente facilitando a integração com
    o Tanstack Form
    
    Então o hook 'useTime' e o componente 'TimeFieldsetRoot' foram atualizados,
    adicionado uma nova propriedade chamada 'value'

--- packages/ui/core/src/components/TimeFieldset/TimeFieldsetRoot.tsx
+++ packages/ui/core/src/components/TimeFieldset/TimeFieldsetRoot.tsx
@@ -1,17 +1,19 @@
+import type { Time, TimeInput } from '@utils/time'
 import { TimeFieldsetContext } from '../../contexts/TimeFieldsetContext'
 import { FieldsetRoot } from '../Fieldset/FieldsetRoot'
-import type { UseTimeInput } from '~/hooks/useTime'
+
 import { useTime } from '~/hooks/useTime'
 
-export interface TimeFieldsetRootProps extends UseTimeInput {
+export interface TimeFieldsetRootProps {
+    value?: TimeInput | Time
     children?: React.ReactNode
 }
 
 export function TimeFieldsetRoot({
-    children,
-    ...props
+    value,
+    children
 }: TimeFieldsetRootProps): React.JSX.Element {
-    const { time, setTime, updateTime } = useTime(props)
+    const { time, setTime, updateTime } = useTime({ value })
 
     return (
         <TimeFieldsetContext value={{ time, setTime, updateTime }}>
