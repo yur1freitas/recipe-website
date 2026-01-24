95027c7143b4711e3ad00c5b6b62b76cbe546be0
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-23 21:48:03 -0300

    feat(ui/dnd): exporte os tipos de evento do `@dnd-kit/core`
    Devido a uma necessidade de ter que usar o tipo dos eventos fora dos
    pacotes, os tipos foram exportados de '@dnd-kit/core' para poderem serem
    reaproveitados sem a necessidade de instalar o pacote que o possui

--- packages/ui/dnd/src/index.ts
+++ packages/ui/dnd/src/index.ts
@@ -1,5 +1,6 @@
 export type { SortableItemContextValue } from './contexts/SortableItemContext'
 export type { SortableProps, Axis, SortableItems } from './components/Sortable'
+export type { DragEvent } from './types'
 
 export { SortableItemContext } from './contexts/SortableItemContext'
 export { useSortableItem } from './hooks/useSortableItemContext'
