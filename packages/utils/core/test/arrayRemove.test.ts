66c184091583cf47d18028ab575a371b5b04ff5b
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-23 21:02:31 -0300

    test(utils/core): implemente testes para `arrayRemove`

---
+++ packages/utils/core/test/arrayRemove.test.ts
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker/locale/pt_BR'
+import { describe, it, expect } from 'vitest'
+
+import { arrayRemove } from '~/arrayRemove'
+
+describe('arrayRemove', () => {
+    it('deve remover um item no array em um índice específico', () => {
+        const array = [1, 2, 3]
+
+        const index = faker.number.int({ min: 0, max: array.length - 1 })
+        const value = array[index]
+
+        const newArray = arrayRemove(array, index)
+
+        expect(array[index]).toBe(value)
+        expect(newArray[index]).not.toBe(value)
+    })
+
+    it('deve retornar uma cópia do array ao remover um item', () => {
+        const array = [1, 2, 3]
+
+        const index = faker.number.int({ min: 0, max: array.length - 1 })
+
+        const newArray = arrayRemove(array, index)
+
+        expect(newArray).not.toBe(array)
+    })
+})
