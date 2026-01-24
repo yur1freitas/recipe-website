eab65d134f33ce8e18a1b19a7ac492182cee804f
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-23 21:01:54 -0300

    test(utils/core): implemente testes para `arrayMove`

---
+++ packages/utils/core/test/arrayMove.test.ts
@@ -0,0 +1,31 @@
+import { faker } from '@faker-js/faker/locale/pt_BR'
+import { describe, it, expect } from 'vitest'
+
+import { arrayMove } from '~/arrayMove'
+
+describe('arrayMove', () => {
+    it('deve mover um item do array para um índice específico', () => {
+        const array = [1, 2, 3]
+        const [a, b] = faker.helpers.uniqueArray(
+            () => faker.number.int({ min: 0, max: array.length - 1 }),
+            2
+        )
+
+        const newArray = arrayMove(array, a, b)
+
+        expect(newArray[a]).not.toBe(array[a])
+        expect(newArray[b]).toBe(array[a])
+    })
+
+    it('deve retornar uma cópia do array ao mover os items', () => {
+        const array = [1, 2, 3]
+        const [a, b] = faker.helpers.uniqueArray(
+            () => faker.number.int({ min: 0, max: array.length - 1 }),
+            2
+        )
+
+        const newArray = arrayMove(array, a, b)
+
+        expect(newArray).not.toBe(array)
+    })
+})
