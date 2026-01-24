739cf45c1e249724017541d2d6eab4b4b3e89e5f
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-23 21:01:33 -0300

    test(utils/core): implemente testes para `arrayInsert`

---
+++ packages/utils/core/test/arrayInsert.test.ts
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker/locale/pt_BR'
+import { describe, it, expect } from 'vitest'
+
+import { arrayInsert } from '~/arrayInsert'
+
+describe('arrayInsert', () => {
+    it('deve inserir um item no array em um índice específico', () => {
+        const array = [1, 2, 3]
+
+        const index = faker.number.int({ min: 0, max: array.length - 1 })
+        const value = faker.number.int({ min: 4, max: 10 })
+
+        const newArray = arrayInsert(array, index, value)
+
+        expect(array[index]).not.toBe(value)
+        expect(newArray[index]).toBe(value)
+    })
+
+    it('deve retornar uma cópia do array ao inserir um item', () => {
+        const array = [1, 2, 3]
+
+        const index = faker.number.int({ min: 0, max: array.length - 1 })
+        const value = faker.number.int({ min: 4, max: 10 })
+
+        const newArray = arrayInsert(array, index, value)
+
+        expect(newArray).not.toBe(array)
+    })
+})
