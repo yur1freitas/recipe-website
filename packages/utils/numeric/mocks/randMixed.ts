87db106f4a3b560c1d7e49a729e8fe56603c3a3f
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-25 21:52:16 -0300

    test(utils/numeric): corrija a geração de fração em `randMixed`
    As frações geradas em 'randMixed' poderia ser negativas, o que não é
    considerado válido no padrão definido no regex
    
    Por isso a função 'randFraction' foi substituída por uma geração usando
    o '@faker-js/faker' para garantir que o numerador e o denominador
    sejam positivos, e depois construir a fração

--- packages/utils/numeric/mocks/randMixed.ts
+++ packages/utils/numeric/mocks/randMixed.ts
@@ -1,6 +1,10 @@
-import { randFraction } from './randFraction'
+import { faker } from '@faker-js/faker/locale/pt_BR'
+
 import { randInt } from './randInt'
 
 export function randMixed(): string {
-    return `${randInt()} ${randFraction()}`
+    const numerator = faker.number.int({ min: 0, max: 100 })
+    const denominator = faker.number.int({ min: 1, max: 100 })
+
+    return `${randInt()} ${numerator}/${denominator}`
 }
