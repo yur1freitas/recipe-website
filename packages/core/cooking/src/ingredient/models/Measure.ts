aeb49b5d77232cf6c20a1140846981d63b9b0075
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-26 12:39:10 -0300

    feat(core/cooking)!: adote o uso de `Numeric` em `Measure`
    A implementação do objeto de valor 'Measure' permanecia incompleta,
    porque ainda não validava a entrada corretamente e nem permitia
    manipulação do valor, mas após a criação de '@utils/numeric' isso se
    tornou possível
    
    Então o pacote foi adicionado e utilizado para finalizar a implementação
    de 'Measure'

--- packages/core/cooking/src/ingredient/models/Measure.ts
+++ packages/core/cooking/src/ingredient/models/Measure.ts
@@ -1,17 +1,39 @@
+import { Numeric } from '@utils/numeric'
 import z from 'zod'
 
 import { ValueObject, ZodValidator } from '@core/shared'
 
 export const measureSchema = z
-    .string()
-    .trim()
-    .nonempty('A medida não pode ser vazia')
-    .transform((e) => e.replace(/\s+/g, ' '))
+    .instanceof(Numeric)
+    .refine(
+        (measure) => measure.toNumber() > 0,
+        'A medida deve ser maior que zero'
+    )
 
 export const MeasureValidator = new ZodValidator(measureSchema)
 
-export class Measure extends ValueObject<string> {
-    constructor(value: string) {
+export class Measure extends ValueObject<Numeric> {
+    constructor(value: Numeric) {
         super(MeasureValidator, value)
+    }
+
+    get isInt(): boolean {
+        return this.value.isInt
+    }
+
+    get isFloat(): boolean {
+        return this.value.isFloat
+    }
+
+    get isFraction(): boolean {
+        return this.value.isFraction
+    }
+
+    get isMixed(): boolean {
+        return this.value.isMixed
+    }
+
+    toNumber(): number {
+        return this.value.toNumber()
     }
 }
