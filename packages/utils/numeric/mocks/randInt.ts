b228ffa019673f557da0d579d4f485774c23d615
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-26 13:19:58 -0300

    test(utils/numeric): permita gerar casos de teste diferentes em `randInt`

--- packages/utils/numeric/mocks/randInt.ts
+++ packages/utils/numeric/mocks/randInt.ts
@@ -1,5 +1,22 @@
 import { faker } from '@faker-js/faker/locale/pt_BR'
 
-export function randInt(): string {
-    return `${faker.number.int({ min: -100, max: 100 })}`
+export interface RandIntOptions {
+    testCase?: 'zero' | 'negative' | 'positive' | 'any'
+}
+
+export function randInt(options?: RandIntOptions): string {
+    switch (options?.testCase) {
+        case 'zero': {
+            return '0'
+        }
+        case 'negative': {
+            return `-${faker.number.int({ min: 1, max: 100 })}`
+        }
+        case 'positive': {
+            return `${faker.number.int({ min: 1, max: 100 })}`
+        }
+        default: {
+            return `${faker.number.int({ min: -100, max: 100 })}`
+        }
+    }
 }
