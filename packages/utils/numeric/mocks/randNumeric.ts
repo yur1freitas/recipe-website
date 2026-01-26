47b5b1e3bb445dff5e8a62097725108137098533
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-26 13:44:29 -0300

    test(utils/numeric): permita gerar casos de teste diferentes em `randNumeric`

--- packages/utils/numeric/mocks/randNumeric.ts
+++ packages/utils/numeric/mocks/randNumeric.ts
@@ -5,11 +5,15 @@ import { randFloat } from './randFloat'
 import { randMixed } from './randMixed'
 import { randInt } from './randInt'
 
-export function randNumeric(): string {
+export interface RandNumericOptions {
+    testCase?: 'negative' | 'positive' | 'any'
+}
+
+export function randNumeric(options?: RandNumericOptions): string {
     return faker.helpers.arrayElement([
         randInt,
         randFloat,
         randFraction,
         randMixed
-    ])()
+    ])(options)
 }
