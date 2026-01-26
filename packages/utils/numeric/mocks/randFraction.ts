71c461052fd92880da185966c50eadb500e1a267
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-26 13:33:04 -0300

    test(utils/numeric): permita gerar casos de teste diferentes em `randFraction`

--- packages/utils/numeric/mocks/randFraction.ts
+++ packages/utils/numeric/mocks/randFraction.ts
@@ -1,12 +1,41 @@
 import { faker } from '@faker-js/faker/locale/pt_BR'
 
-export function randFraction(): string {
-    const numerator = faker.number.int({ min: -100, max: 100 })
+export interface RandFractionOptions {
+    testCase?: 'denominator-zero' | 'negative' | 'positive' | 'any'
+}
 
-    const denominator = faker.helpers.arrayElement([
-        faker.number.int({ min: 1, max: 100 }),
-        faker.number.int({ min: -100, max: -1 })
-    ])
+export function randFraction(options?: RandFractionOptions): string {
+    switch (options?.testCase) {
+        case 'denominator-zero': {
+            const numerator = faker.number.int({ min: -100, max: 100 })
+            const denominator = '0'
 
-    return `${numerator}/${denominator}`
+            return `${numerator}/${denominator}`
+        }
+        case 'negative': {
+            const [a, b] = faker.helpers.multiple(
+                () => faker.number.int({ min: 1, max: 100 }),
+                { count: 2 }
+            )
+
+            return faker.helpers.shuffle([-a, b]).join('/')
+        }
+        case 'positive': {
+            return faker.helpers
+                .multiple(() => faker.number.int({ min: 1, max: 100 }), {
+                    count: 2
+                })
+                .join('/')
+        }
+        default: {
+            const numerator = faker.number.int({ min: -100, max: 100 })
+
+            const denominator = faker.helpers.arrayElement([
+                faker.number.int({ min: 1, max: 100 }),
+                faker.number.int({ min: -100, max: -1 })
+            ])
+
+            return `${numerator}/${denominator}`
+        }
+    }
 }
