7dd49d22ccee407af5ffcab02a3026330e8a8d5f
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-26 13:15:58 -0300

    test(utils/numeric): permitar gerar casos de teste diferentes em `randFloat`

--- packages/utils/numeric/mocks/randFloat.ts
+++ packages/utils/numeric/mocks/randFloat.ts
@@ -1,5 +1,31 @@
 import { faker } from '@faker-js/faker/locale/pt_BR'
 
-export function randFloat(): string {
-    return `${faker.number.int({ min: 0, max: 100 })}.${faker.number.int({ min: 0, max: 100 })}`
+export interface RandFloatOptions {
+    testCase?: 'zero' | 'negative' | 'positive' | 'any'
+}
+
+export function randFloat(options?: RandFloatOptions): string {
+    switch (options?.testCase) {
+        case 'zero': {
+            return '0.0'
+        }
+        case 'negative': {
+            const whole = faker.number.int({ min: 0, max: 100 })
+            const float = faker.number.int({ min: 1, max: 100 })
+
+            return `-${whole}.${float}`
+        }
+        case 'positive': {
+            const whole = faker.number.int({ min: 0, max: 100 })
+            const float = faker.number.int({ min: 1, max: 100 })
+
+            return `${whole}.${float}`
+        }
+        default: {
+            const whole = faker.number.int({ min: -100, max: 100 })
+            const float = faker.number.int({ min: 1, max: 100 })
+
+            return `${whole}.${float}`
+        }
+    }
 }
