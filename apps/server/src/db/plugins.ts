de1b93782a514e76abacbbb4d285b2113653bbec
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-27 22:04:48 -0300

    feat(apps/server): crie um plugin para converter a coluna `measure` em `Numeric`
    Para facilitar as consultas ao banco de dados e evitar a necessidade de
    ter que converter a coluna 'measure' toda vez, um plugin foi feito para
    permitir que essa conversão seja automática e também aplicada somente às
    colunas de tabelas específicas

--- apps/server/src/db/plugins.ts
+++ apps/server/src/db/plugins.ts
@@ -1,4 +1,6 @@
 import type {
+    TableNode,
+    QueryId,
     KyselyPlugin,
     PluginTransformQueryArgs,
     PluginTransformResultArgs,
@@ -8,6 +10,7 @@ import type {
 } from 'kysely'
 
 import { Time } from '@utils/time'
+import { Numeric } from '@utils/numeric'
 
 export interface TimePluginOptions {
     columns?: string[]
@@ -53,5 +56,79 @@ export class TimePlugin implements KyselyPlugin {
         }
 
         return args.result
+    }
+}
+
+export interface NumericPluginOptions {
+    identifiers: { table: string; columns: string[] }[]
+}
+
+export class NumericPlugin implements KyselyPlugin {
+    private $data: WeakMap<QueryId, string[]> = new WeakMap()
+
+    private $identifiers: Map<string, string[]>
+
+    constructor({ identifiers }: NumericPluginOptions) {
+        const entries: [string, string[]][] = identifiers.map((item) => [
+            item.table,
+            item.columns
+        ])
+
+        this.$identifiers = new Map(entries)
+    }
+
+    transformQuery(args: PluginTransformQueryArgs): RootOperationNode {
+        if (args.node.kind === 'SelectQueryNode' && args.node.from) {
+            for (const fromNode of args.node.from.froms) {
+                if (fromNode.kind === 'TableNode') {
+                    const tableNode = fromNode as TableNode
+                    const tableName = tableNode.table.identifier.name
+
+                    if (this.$identifiers.has(tableName)) {
+                        const columns = this.$identifiers.get(tableName)!
+
+                        this.$data.set(args.queryId, columns)
+                    }
+                }
+            }
+        }
+
+        return args.node
+    }
+
+    async transformResult(
+        args: PluginTransformResultArgs
+    ): Promise<QueryResult<UnknownRow>> {
+        const columns = this.$data.get(args.queryId)
+
+        if (columns && args.result.rows && Array.isArray(args.result.rows)) {
+            const rows = args.result.rows.map((row) => {
+                const updatedRow = { ...row }
+
+                for (const column of columns) {
+                    if (Object.hasOwn(row, column)) {
+                        const value = updatedRow[column]
+
+                        if (typeof value === 'string') {
+                            updatedRow[column] = this.parseValue(value)
+                        }
+                    }
+                }
+
+                return updatedRow
+            })
+
+            return {
+                ...args.result,
+                rows
+            }
+        }
+
+        return args.result
+    }
+
+    protected parseValue(value: string): Numeric {
+        const cleanValue = value.replace(/"+/g, '')
+        return new Numeric(cleanValue)
     }
 }
