be8cb2b0d9aca79c506c5104aa8a167ccabb873f
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-29 17:07:53 -0300

    refactor(apps/server): separe a versão do fastify dos IPs em `listeningInfo`

--- apps/server/src/utils/info.ts
+++ apps/server/src/utils/info.ts
@@ -3,7 +3,7 @@ import type { FastifyInstance } from 'fastify'
 export function listeningInfo(app: FastifyInstance): void {
     const fastifyVersion = `🗲 Fastify ${app.version}`
 
-    console.info('\x1b[36m%s\x1b[0m', fastifyVersion)
+    console.info('\x1b[36m%s\x1b[0m\n', fastifyVersion)
 
     for (const address of app.addresses()) {
         const addressFamily = `- ${address.family}:`
