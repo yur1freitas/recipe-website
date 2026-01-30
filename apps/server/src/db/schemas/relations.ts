08a49d6ccffe233c634731009a00290782daf82f
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-01-30 14:31:38 -0300

    fix(apps/server): configure as chaves estrangeiras nos schemas de relacionamento
    Na definição de relacionamentos em 'db/schemas/relations.ts' faltou a
    configuração das colunas e suas respectivas referências que acabou
    causando erros ao tentar realizar algumas operações no banco de dados
    
    Dito isso foi feito a devida configuração para que resolve-se o problema

--- apps/server/src/db/schemas/relations.ts
+++ apps/server/src/db/schemas/relations.ts
@@ -6,15 +6,24 @@ import { recipesTable } from './recipes'
 import { ingredientsTable } from './ingredients'
 
 export const stepsRelations = relations(stepsTable, ({ one }) => ({
-    recipe: one(recipesTable)
+    recipe: one(recipesTable, {
+        fields: [stepsTable.recipeId],
+        references: [recipesTable.id]
+    })
 }))
 
 export const toolsRelations = relations(toolsTable, ({ one }) => ({
-    recipe: one(recipesTable)
+    recipe: one(recipesTable, {
+        fields: [toolsTable.recipeId],
+        references: [recipesTable.id]
+    })
 }))
 
 export const ingredientsRelations = relations(ingredientsTable, ({ one }) => ({
-    recipe: one(recipesTable)
+    recipe: one(recipesTable, {
+        fields: [ingredientsTable.recipeId],
+        references: [recipesTable.id]
+    })
 }))
 
 export const recipesRelations = relations(recipesTable, ({ many }) => ({
