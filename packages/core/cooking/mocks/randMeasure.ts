e0a1b61ac15507d47f81c90e5a02f4141229455d
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-02-05 17:57:00 -0300

    test(core/cooking): retorne `string` e não `Numeric` em `randMeasure`

--- packages/core/cooking/mocks/randMeasure.ts
+++ packages/core/cooking/mocks/randMeasure.ts
@@ -1,20 +1,19 @@
 import { randNumeric } from '@utils/numeric/mocks'
-import { Numeric } from '@utils/numeric'
 
 export interface RandMeasureOptions {
     testCase?: 'zero' | 'negative' | 'success'
 }
 
-export function randMeasure(options?: RandMeasureOptions): Numeric {
+export function randMeasure(options?: RandMeasureOptions): string {
     switch (options?.testCase) {
         case 'zero': {
-            return new Numeric('0')
+            return '0'
         }
         case 'negative': {
-            return new Numeric(randNumeric({ testCase: 'negative' }))
+            return randNumeric({ testCase: 'negative' })
         }
         default: {
-            return new Numeric(randNumeric({ testCase: 'positive' }))
+            return randNumeric({ testCase: 'positive' })
         }
     }
 }
