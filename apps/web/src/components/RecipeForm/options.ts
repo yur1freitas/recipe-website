d7acef28c215056a1fd5b8c6dec1a08ac38fb7e7
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-02-05 18:48:45 -0300

    fix(apps/web): atualize o valor padrão de `preparationTime` em `recipeFormOptions`
    Referente ao commit 52150c2cb36ba1cf7a95d165fb4a8d004a58ee4e, o valor de
    entrada usado no schema deixou de ser uma instância de 'Time' e passou a
    ser um 'number'
    
    Então o valor padrão foi atualizado para evitar problemas

--- apps/web/src/components/RecipeForm/options.ts
+++ apps/web/src/components/RecipeForm/options.ts
@@ -1,21 +1,20 @@
 import type { infer as ZodInfer } from 'zod'
 
-import { Time } from '@utils/time'
 import { formOptions } from '@tanstack/react-form'
 
 import { DifficultyEnum, recipeSchema } from '@core/cooking'
+
+const schema = recipeSchema.omit({ id: true, authorId: true })
 
 const DEFAULT_VALUES: ZodInfer<typeof schema> = {
     name: '',
     description: '',
     difficulty: DifficultyEnum.EASY,
-    preparationTime: new Time(),
+    preparationTime: 0,
     steps: [],
     tools: [],
     ingredients: []
 }
-
-const schema = recipeSchema.omit({ id: true, authorId: true })
 
 export const recipeFormOptions = formOptions({
     defaultValues: DEFAULT_VALUES,
