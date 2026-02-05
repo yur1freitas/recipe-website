5780d2dbb53f352bac07fa0e14b9556fe16f7b29
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-02-05 18:39:09 -0300

    feat(core/cooking): adicione exemplos nos metadados de `nameSchema`

--- packages/core/cooking/src/shared/models/Name.ts
+++ packages/core/cooking/src/shared/models/Name.ts
@@ -9,6 +9,9 @@ export const nameSchema = z
     .min(3, 'O nome deve ter no mínimo 3 caracteres')
     .max(64, 'O nome deve ter no máximo 64 caracteres')
     .regex(/^[\p{L}\p{M}\d\s-]+$/iu, 'O nome deve ser válido')
+    .meta({
+        examples: ['Tijela', 'Pão de Queijo', 'Farinha']
+    })
 
 export const NameValidator = new ZodValidator(nameSchema)
 
