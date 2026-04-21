94a55c3109a1c38a89ce36dda69847516ec5ac3e
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-04-21 18:37:37 -0300

    feat(apps/server): adicione a opção de recuperar dados do usuário através da verificação do token
    A api não oferecia nenhuma rota ou opção para recuperar dados do usuário
    para ser exibida na interface do usuário, como nome e email
    
    Então na rota '/auth/verify' foi adicionado um parâmetro de busca que
    permite retornar o conteúdo do 'accessToken' enviado via cookies, mas apenas as
    propriedades 'id', 'name' e 'email' são retornados

--- apps/server/src/api/controllers/auth/verifyUserSessionController.ts
+++ apps/server/src/api/controllers/auth/verifyUserSessionController.ts
@@ -6,6 +6,11 @@ import type { RawServerDefault } from 'fastify'
 
 import fp from 'fastify-plugin'
 
+import type { UserPayload } from '@core/auth'
+
+import { emailSchema, idSchema } from '@core/shared'
+import { nameSchema } from '@core/cooking'
+
 import { serverErrorSchema } from '~/schemas/serverErrorSchema'
 import { httpErrorSchema } from '~/schemas/httpErrorSchema'
 
@@ -20,10 +25,22 @@ export const verifyUserSessionController = fp<
             schema: {
                 tags: ['auth'],
                 description: 'Verificar a sessão do usuário',
+                querystring: z.object({
+                    payload: z.coerce.boolean<boolean>().default(false)
+                }),
                 cookies: {
                     accessToken: z.string().optional()
                 },
                 response: {
+                    200: z
+                        .object({
+                            id: idSchema,
+                            name: nameSchema,
+                            email: emailSchema
+                        })
+                        .describe(
+                            'A sessão do usuário é válida e o payload foi retornado'
+                        ),
                     204: z.undefined().describe('A sessão do usuário é válida'),
                     401: httpErrorSchema.describe(
                         'A sessão do usuário é inválida'
@@ -33,7 +50,15 @@ export const verifyUserSessionController = fp<
             },
             preHandler: [app.pasetoHandler()]
         },
-        async (_, reply) => {
+        async (request, reply) => {
+            const { payload } = request.query
+
+            if (payload) {
+                const { id, name, email } = request.tokenPayload as UserPayload
+
+                return reply.status(200).send({ id, name, email })
+            }
+
             return reply.status(204).send()
         }
     )
