977dcd631b78a413028f9181effb27db16b49faf
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-04-21 19:38:27 -0300

    feat(core/auth)!: defina o retorno de `update` como `boolean` em `UserRepositoryProvider`
    Em 'UserRepositoryProvider', métodos como 'create' e 'delete' retornam
    'boolean' que indicam se a operação aconteceu ou não com sucesso, mas o
    método 'update' não retorna isso
    
    Então o tipo do retorno do método 'update' foi alterado de 'void' para
    'boolean' para permitir que o caso de uso lançe um erro se o provedor
    falhar

--- packages/core/auth/src/user/providers/UserRepositoryProvider.ts
+++ packages/core/auth/src/user/providers/UserRepositoryProvider.ts
@@ -5,7 +5,7 @@ import type { User } from '../models/User'
 export interface UserRepositoryProvider {
     create(user: User): Awaitable<boolean>
     delete(id: string): Awaitable<boolean>
-    update(user: User): Awaitable<void>
+    update(user: User): Awaitable<boolean>
     findAll(): Awaitable<User[]>
     findById(id: string): Awaitable<User | null>
     findByEmail(email: string): Awaitable<User | null>
