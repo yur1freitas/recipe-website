ce27c7e59735fd7ea876389764c44ea140c3a8ee
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-04-22 13:16:51 -0300

    fix(apps/server): atualize `PasetoAdapter` para refletir as mudanças em `AccessTokenProvider`
    Referente ao commit 4c33176aa62994807f811cba0f35d5cab06abd5d, o conteúdo
    do token de acesso passou a ser definido pela interface
    'AccessTokenPayload' em vez de 'UserPayload'
    
    Então a interface 'UserPayload' foi substituída por 'AccessTokenPayload'
    e o conteúdo do token retornado após a decodificação foi adaptado para
    refletir essa mudança

--- apps/server/src/adapters/auth/PasetoAdapter.ts
+++ apps/server/src/adapters/auth/PasetoAdapter.ts
@@ -1,11 +1,11 @@
 import { sign, verify } from 'paseto-ts/v4'
 
-import type { AccessTokenProvider, UserPayload } from '@core/auth'
+import type { AccessTokenPayload, AccessTokenProvider } from '@core/auth'
 
 import { env } from '~/env'
 
 export class PasetoAdapter implements AccessTokenProvider {
-    create(payload: UserPayload): string {
+    create(payload: AccessTokenPayload): string {
         return sign(
             env.TOKEN_PRIVATE_KEY,
             {
@@ -26,21 +26,21 @@ export class PasetoAdapter implements AccessTokenProvider {
         )
     }
 
-    decode(token: string): UserPayload {
-        const { payload } = verify<UserPayload>(env.TOKEN_PUBLIC_KEY, token, {
-            validatePayload: true,
-            assertion: {
-                iss: env.TOKEN_ISSUER,
-                aud: env.TOKEN_AUDIENCE,
-                exp: env.TOKEN_EXPIRATION
+    decode(token: string): AccessTokenPayload {
+        const { payload } = verify<AccessTokenPayload>(
+            env.TOKEN_PUBLIC_KEY,
+            token,
+            {
+                validatePayload: true,
+                assertion: {
+                    iss: env.TOKEN_ISSUER,
+                    aud: env.TOKEN_AUDIENCE,
+                    exp: env.TOKEN_EXPIRATION
+                }
             }
-        })
+        )
 
-        return {
-            id: payload.id,
-            name: payload.name,
-            email: payload.email
-        }
+        return { userId: payload.userId }
     }
 
     verify(token: string): boolean {
